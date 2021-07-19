6e6e01d525420eefde0537132a3bc2ca1cef384f
Arsen Mikayelyan <arsen.mikayelyan@teamviewer.com>
2021-07-19 12:05:23 +0400

    combination added

---
+++ com/src/main/java/combination/Combination.java
@@ -0,0 +1,31 @@
+package combination;
+
+import java.util.Scanner;
+
+class Combination {
+
+    public static void combination(int arr[], int data[], int start, int end, int index, int r) {
+        if (index == r) {
+            for (int j = 0; j < r; j++)
+                System.out.print(data[j] + " ");
+            System.out.println();
+            return;
+        }
+        for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
+            data[index] = arr[i];
+            combination(arr, data, i + 1, end, index + 1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sr.nextInt();
+        }
+        int r = sr.nextInt();
+        int data[] = new int[r];
+        combination(a, data, 0, n - 1, 0, r);
+    }
+}
